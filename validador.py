2ba8b9edf06d77c8f4c3aeb46fbe4265c9f80960
BiaBaccaro <130668481+BiaBaccaro@users.noreply.github.com>
2023-10-16 16:23:07 -0300

    traingulo agora

---
+++ validador.py
@@ -0,0 +1,22 @@
+# VALIDAR UM TRIANGULO
+
+#entrada
+a=int(input('digite um valorpara o lado do triângulo'))
+b=int(input('digite um valorpara o lado do triângulo'))
+c=int(input('digite um valorpara o lado do triângulo'))
+
+#processamento e saída dfhsdghsgh
+
+if a + b < c or b + c < a or c + b < a:
+    print('não é uma triangulo')
+else:
+    print('é um triangulo')
+    if a==b and b==c and c==a:
+    print('é equilátero')
+    if a==b or b==c or c==a:
+    print('é um triangulo Isósceles ')
+
+
+
+
+
