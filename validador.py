19d966e02139088dfefe33197a8c6e84a308c5a5
anaClara3112 <130669098+anaClara3112@users.noreply.github.com>
2023-10-17 14:10:28 -0300

    - Commit inicial

--- validador.py
+++ validador.py
@@ -1,9 +1,9 @@
 # VALIDAR UM TRIANGULO
 
 #entrada
-a=int(input('digite um valorpara o lado do triângulo'))
-b=int(input('digite um valorpara o lado do triângulo'))
-c=int(input('digite um valorpara o lado do triângulo'))
+a=int(input('Digite o valor de um lado do triângulo:'))
+b=int(input('Digite o valor do outro lado do triângulo:'))
+c=int(input('Digite o valor do último lado do triângulo:'))
 
 #processamento e saída dfhsd
 
@@ -12,11 +12,8 @@ if a + b < c or b + c < a or c + b < a:
 else:
     print('é um triangulo')
     if a==b and b==c and c==a:
-    print('é equilátero')
-    if a==b or b==c or c==a:
-    print('é um triangulo Isósceles ')
-
-
-
-
-
+        print('É equilátero')
+    if a==b  and a!=c and b!=c or b==c and a!=b and a!=c or c==a and a!=b and b!=c:
+        print('É um triangulo Isósceles ')
+    if a!=b and a!=c and b!=c:
+        print('É um triângulo Escaleno')
